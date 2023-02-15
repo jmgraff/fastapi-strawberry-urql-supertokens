361b0e171c449fa1eb118aca6175b562cbf8a767
John Graff <john@codehammer.io>
2023-02-15 16:47:05 -0500

    move supertokens config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,26 @@ import { Provider } from "urql";
 import { urqlClient } from "./utils";
 import { BrowserRouter } from "react-router-dom";
 
+import SuperTokens from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
 import { SuperTokensWrapper } from "supertokens-auth-react";
 
 import Main from "./Main";
+
+SuperTokens.init({
+    appInfo: {
+        appName: "example",
+        apiDomain: `http://${process.env.REACT_APP_HOSTNAME}:8000`,
+        websiteDomain: `http://${process.env.REACT_APP_HOSTNAME}:3000`,
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth",
+    },
+    recipeList: [
+        EmailPassword.init(),
+        Session.init()
+    ]
+});
 
 
 function App() {
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,21 +1,4 @@
 import { createClient, defaultExchanges } from "urql";
-import SuperTokens from "supertokens-auth-react";
-import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
-import Session from "supertokens-auth-react/recipe/session";
-
-SuperTokens.init({
-    appInfo: {
-        appName: "example",
-        apiDomain: `http://${process.env.REACT_APP_HOSTNAME}:8000`,
-        websiteDomain: `http://${process.env.REACT_APP_HOSTNAME}:3000`,
-        apiBasePath: "/auth",
-        websiteBasePath: "/auth",
-    },
-    recipeList: [
-        EmailPassword.init(),
-        Session.init()
-    ]
-});
 
 export const urqlClient = createClient({
     url: `http://${process.env.REACT_APP_HOSTNAME}:8000/graphql`,
